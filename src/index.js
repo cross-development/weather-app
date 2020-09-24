785199ab7fe99a6d87dba567580c11c44b342eaa
Vitaliy Derda <derdavitaliy@gmail.com>
2020-09-24 08:10:44 +0300

    added project structure

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//Core
+import React from 'react';
+import ReactDOM from 'react-dom';
+//Components
+import App from 'components/App';
+//Styles
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
