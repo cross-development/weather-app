b78e1f29667c2193a680d103c4d83e9c5314c889
Vitaliy Derda <derdavitaliy@gmail.com>
2020-10-04 11:49:10 +0300

    remove comments

--- src/services/weatherAPI.js
+++ src/services/weatherAPI.js
@@ -18,8 +18,6 @@ export default {
 			});
 	},
 
-	fetchCurrentWeatherByGPS() {},
-
 	fetchForecastWeather() {
 		return axios
 			.get(`forecast/daily?city=${this.query},${this.code}`, { params })
@@ -43,37 +41,3 @@ export default {
 		this.code = code;
 	},
 };
-
-// axios.defaults.baseURL = 'http://api.weatherstack.com/';
-// const params = { access_key: '2623b33751e9d4766243ed71636880f3' };
-
-// export default {
-// 	query: '',
-
-// fetchCurrentWeatherByQuery() {
-// 	return axios
-// 		.get(`current?units=m&query=${this.query}`, { params })
-// 		.then(({ data }) => data)
-// 		.catch(error => {
-// 			throw Error(404);
-// 		});
-// },
-
-// 	fetchCurrentWeatherByGPS() {},
-
-// 	fetchForecastWeather() {
-// 		axios
-// 			.get(`forecast?units=m&forecast_days=1&hourly=1&query=${this.query}`, { params })
-// 			.then(({ data }) => data)
-// 			.catch(error => {
-// 				throw Error(404);
-// 			});
-// 	},
-
-// 	/**
-// 	 * @param {string} query
-// 	 */
-// 	set searchQuery(query) {
-// 		this.query = query;
-// 	},
-// };
