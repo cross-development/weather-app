c0587c2b5fd14e169b664a25d5af35df4d72cc93
Vitaliy Derda <derdavitaliy@gmail.com>
2020-11-24 16:45:10 +0200

    Updates

---
+++ precache-manifest.a584af3ecec8b9f515089df095f843be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560ddd472dd494c53635c02639f4d39b",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "fa557b6a15c0984c3df6",
+    "url": "/weather-app/static/css/main.00accee8.chunk.css"
+  },
+  {
+    "revision": "99bb18b56f8b7189f193",
+    "url": "/weather-app/static/js/2.42a6e8df.chunk.js"
+  },
+  {
+    "revision": "9f90cde4396c52b73cc0a6bcd8267b7b",
+    "url": "/weather-app/static/js/2.42a6e8df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa557b6a15c0984c3df6",
+    "url": "/weather-app/static/js/main.5cff180f.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "8ac1cd3d0c104c423a4a943f6cd50a50",
+    "url": "/weather-app/static/media/9129060qwe.8ac1cd3d.jpg"
+  }
+]);
